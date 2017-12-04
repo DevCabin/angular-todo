0a2fdd69e4f110901e9e5f36e5963b02d713d510
Dillan <dillan.arzate@gmail.com>
2017-12-04 10:50:09 -0600

    Updated File

--- js/controller.js
+++ js/controller.js
@@ -33,21 +33,7 @@ angular.module('RouteControllers', [])
                 }).catch(function(err) {
                 	alert("Oops! Something went wrong!");
                 	console.log(err);
-                });
-            }
-
-            $scope.submitForm=function() {
-                if ($scope.registrationForm.$valid) {
-                    $scope.registrationUser.username=$scope.user.username;
-                    $scope.registrationUser.password=$scope.user.password;
-
-                    UserAPIService.callAPI(URL+"accounts/register/", $scope.registrationUser).then(function(results) {
-                        $scope.data=results.data;
-                        alert("You have successfully registered to Angular Todo");
-                        $scope.login();
-                    }).catch(function(err) {
-                        alert("Oops! Something went wrong!");
-                        console.log(err);
+                    
                     });
                 }
             }
