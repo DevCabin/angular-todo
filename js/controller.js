1b334a5884ef5d573893861e45b4dbdffc8d17ef
Dillan <dillan.arzate@gmail.com>
2017-12-04 11:01:00 -0600

    Fixed

--- js/controller.js
+++ js/controller.js
@@ -1,22 +1,16 @@
 angular.module('RouteControllers', [])
     .controller('HomeController', function($scope) {
-        $scope.title = "Welcome To Angular Todo!";
+        $scope.title = "Welcome To Angular Todo!"
     })
     .controller('RegisterController', function($scope, UserAPIService, store) {
- 
         $scope.registrationUser = {};
         var URL = "https://morning-castle-91468.herokuapp.com/";
-
-        var authStorage = {
-            name: "StorageTest"
-        };
-
-        store.set('obj', authStorage);
-
+ 
         $scope.login = function() {
-            UserAPIService.callAPI(URL + "accounts/api-token-auth", $scope.data).then(function(results) {
+            UserAPIService.callAPI(URL + "accounts/api-token-auth/",$scope.data).then(function(results) {
                 $scope.token = results.data.token;
-                console.log($scope.token);
+                store.set('username', $scope.registrationUser.username);
+                store.set('authToken', $scope.token);
             }).catch(function(err) {
                 console.log(err.data);
             });
@@ -26,16 +20,14 @@ angular.module('RouteControllers', [])
             if ($scope.registrationForm.$valid) {
                 $scope.registrationUser.username = $scope.user.username;
                 $scope.registrationUser.password = $scope.user.password;
-
-                UserAPIService.registerUser(URL + "accounts/register/", $scope.registrationUser).then(function(results) {
-                	$scope.data = results.data;
-                	alert("You have successfully registered to Angular Todo");
+ 
+                UserAPIService.callAPI(URL + "accounts/register/",$scope.registrationUser).then(function(results) {
+                    $scope.data = results.data;
+                    alert("You have successfully registered to Angular Todo");
+                    $scope.login();
                 }).catch(function(err) {
-                	alert("Oops! Something went wrong!");
-                	console.log(err);
-                    
-                    });
-                }
+                    alert("Oops! Something went wrong!");
+                });
             }
         };
     });
